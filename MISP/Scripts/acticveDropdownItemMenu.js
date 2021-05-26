213f6c12597a1fde73ab42cb33e2a010f9893f06
GuD1mon <89131859292fhntv14@mail.ru>
2021-05-27 02:52:02 +0700

    v2.8 newBranch

---
+++ MISP/Scripts/acticveDropdownItemMenu.js
@@ -0,0 +1,20 @@
+﻿$('.dropdown-item').click(function () {
+    let idDropdownItem = this.id
+    localStorage.setItem("idActiveItem", idDropdownItem)
+})
+
+
+$(document).ready(function () {
+    let dropdownItems = document.getElementsByClassName('dropdown-item');
+    let idActiveItem = localStorage.getItem('idActiveItem')
+    for (let i = 0; i < dropdownItems.length; i++) {
+        dropdownItems[i].classList.remove('active');
+    }
+    for (let i = 0; i < dropdownItems.length; i++) {
+        if (dropdownItems[i].id == idActiveItem) {
+            dropdownItems[i].classList.add('active');
+        }
+    }
+    localStorage.removeItem('idActiveItem')
+
+})
