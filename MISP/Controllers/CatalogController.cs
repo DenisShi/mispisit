ae37baa2a63860b0bef1d88ffb690131fa4957e9
denis <okposhka09@gmail.com>
2021-05-31 19:18:05 +0700

    add catalog, cart

--- MISP/Controllers/CatalogController.cs
+++ MISP/Controllers/CatalogController.cs
@@ -1,25 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
-
 using System.Web.Mvc;
+using MISP;
+using MISP.Rerository;
 
 namespace MISP.Controllers
 {
     public class CatalogController : Controller
     {
-        Catalog Catalog;
-        public ActionResult GetView()
+
+        // GET: Catalog
+        public ActionResult Index()
         {
-            return null;
+            using (Context db = new Context())
+                return View(db.Products.ToList());
         }
 
-        public void SelectProduct(int id)
+        public ActionResult AddToCart(int id)
         {
+            using (Context db = new Context())
+                GetCart().AddItem(db.Products.Find(id), 1);
+            return Redirect("/Catalog");
+        }
 
+        public Cart GetCart()
+        {
+                Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
         }
 
 
+
+
+
+        //// GET: Catalog/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Product product = db.Products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(product);
+        //}
+
+        //// GET: Catalog/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Catalog/Create
+        //// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        //// сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,Name,Price,Description,ImagePath")] Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Products.Add(product);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(product);
+        //}
+
+        //// GET: Catalog/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Product product = db.Products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(product);
+        //}
+
+        //// POST: Catalog/Edit/5
+        //// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        //// сведения см. в разделе https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Name,Price,Description,ImagePath")] Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(product).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(product);
+        //}
+
+        //// GET: Catalog/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Product product = db.Products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(product);
+        //}
+
+        //// POST: Catalog/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Product product = db.Products.Find(id);
+        //    db.Products.Remove(product);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
--- MISP/Models/Cart.cs
+++ MISP/Models/Cart.cs
@@ -4,28 +4,54 @@ using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MISP
 {
     public class Cart
     {
-        public List<CartLine> LineCollection { get; set; }
+        public List<CartLine> Lines = new List<CartLine>();
         public void AddItem(Product product, int quantity)
         {
+            CartLine line = Lines
+               .Where(p => p.Product.Id == product.Id)
+               .FirstOrDefault();
 
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
         }
         public void RemoveLine(Product product)
         {
-
+            Lines.RemoveAll(l => l.Product.Id == product.Id);
         }
 
         public decimal ComputeTotalValue()
         {
-            return 0;
+            return Lines.Sum(e => Convert.ToDecimal(e.Product.Price) * e.Quantity);
         }
         public void Clear()
         {
+            Lines.Clear();
+        }
+    }
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
 
+        public float GetCost()
+        {
+            return Product.Price * Quantity;
         }
     }
 }
